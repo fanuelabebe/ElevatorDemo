21b76c3cee0448424c1958d25cca01b1662e1a87
Fanuel <fanuelabebe@gmail.com>
2022-07-27 17:23:50 +0300

    Authorization Check

---
+++ app/src/main/java/com/fanuel/elevatordemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fanuel.elevatordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //AuthorizationCheck
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
